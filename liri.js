e1df24d35ed9333abdee9d3cc98c271014823703
Liberatone <musictus@gmail.com>
2018-12-08 17:15:26 -0500

    initial files

---
+++ liri.js
@@ -0,0 +1,12 @@
+require("dotenv").config();
+require('node-spotify-api');
+
+var spotify = new Spotify(keys.spotify);
+
+// concert-this
+
+// spotify-this-song
+
+// movie-this
+
+// do-what-it-says
